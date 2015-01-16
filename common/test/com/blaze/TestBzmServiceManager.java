d4e0c7ad6215d55383853c0d09a8a49427983e3c
dzmitrykashlach <dzmitrykashlach@gmail.com>
2015-01-16 17:18:50 +0300

    clean build

---
+++ common/test/com/blaze/TestBzmServiceManager.java
@@ -0,0 +1,14 @@
+package com.blaze;
+
+import org.junit.Test;
+
+/**
+ * Created by dzmitrykashlach on 16/01/15.
+ */
+public class TestBzmServiceManager {
+    @Test
+    public void testBzmServiceManager(){
+        System.out.println("llllllllllllllllllllllllllllllllllll");
+    }
+
+}
