f829e43471deefe57f6444439561e7760b06e5e7
zmicer <dzmitrykashlach@gmail.com>
2016-07-08 13:52:01 +0300

    LICENSE:Apachec 2.0(cherry picked from commit a94e989)

--- common/src/com/blaze/api/urlmanager/BmUrlManagerV3Impl.java
+++ common/src/com/blaze/api/urlmanager/BmUrlManagerV3Impl.java
@@ -1,14 +1,22 @@
+/**
+ Copyright 2016 BlazeMeter Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package com.blaze.api.urlmanager;
 
-import com.blaze.api.TestType;
-import com.blaze.runner.Constants;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-/**
- * Created by dzmitrykashlach on 5/12/14.
- */
 public class BmUrlManagerV3Impl implements BmUrlManager{
 
     private String serverUrl = "";
