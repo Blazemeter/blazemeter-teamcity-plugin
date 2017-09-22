1594823fb5d3801b16b247c1546cc7208f389711
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-09-22 10:46:00 +0300

    update license

--- agent/src/main/java/com/blaze/agent/BzmBuildProcess.java
+++ agent/src/main/java/com/blaze/agent/BzmBuildProcess.java
@@ -1,15 +1,15 @@
 /**
- Copyright 2016 BlazeMeter Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright 2017 BlazeMeter Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.blaze.agent;
@@ -41,7 +41,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class BzmBuildProcess implements BuildProcess {
-    
+
     private static final int CHECK_INTERVAL = 60000;
     private static final int INIT_TEST_TIMEOUT = 180000;
     private com.blaze.agent.BzmBuild bzmBuild;
