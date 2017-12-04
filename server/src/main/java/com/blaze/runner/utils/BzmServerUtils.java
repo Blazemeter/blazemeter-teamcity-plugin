edee7845dda0dd5b3fb98167a221b242b4e84729
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-12-04 15:22:18 +0300

    add server utils controller

--- server/src/main/java/com/blaze/runner/utils/BzmServerUtils.java
+++ server/src/main/java/com/blaze/runner/utils/BzmServerUtils.java
@@ -3,13 +3,11 @@ package com.blaze.runner.utils;
 import com.blaze.runner.logging.BzmServerLogging;
 import com.blaze.runner.logging.BzmUserNotifier;
 import com.blaze.utils.TCBzmUtils;
-import com.blazemeter.api.logging.Logger;
-import com.blazemeter.api.logging.UserNotifier;
 
 public class BzmServerUtils extends TCBzmUtils {
 
-    public BzmServerUtils(String apiKeyId, String apiKeySecret, String address, UserNotifier notifier, Logger logger) {
-        super(apiKeyId, apiKeySecret, address, notifier, logger);
+    public BzmServerUtils(String apiKeyId, String apiKeySecret, String address) {
+        super(apiKeyId, apiKeySecret, address, new BzmUserNotifier(), new BzmServerLogging());
     }
 
     public BzmServerUtils() {
