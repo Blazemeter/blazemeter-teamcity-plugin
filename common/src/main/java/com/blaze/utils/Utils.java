11d0689b783749a241b08bce60780a1cacdad509
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-12-13 18:36:29 +0300

    cleanup utils

--- common/src/main/java/com/blaze/utils/Utils.java
+++ common/src/main/java/com/blaze/utils/Utils.java
@@ -14,25 +14,13 @@
 
 package com.blaze.utils;
 
-import com.intellij.openapi.util.text.StringUtil;
-import jetbrains.buildServer.agent.BuildProgressLogger;
-import jetbrains.buildServer.agent.BuildRunnerContext;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-
-
-import java.io.*;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Utils {
 
-    private static Logger logger = LoggerFactory.getLogger("com.blazemeter");
-
     private Utils() {
     }
-
 
     public static String version() {
         Properties props = new Properties();
@@ -44,67 +32,11 @@ public class Utils {
         return props.getProperty("version");
     }
 
-
     public static String getTestId(String testId) {
         try {
             return testId.substring(0, testId.lastIndexOf("."));
         } catch (Exception e) {
             return testId;
-        }
-    }
-
-    public static File mkReportDir(String defaultRootDir, String reportDir) {
-        File reportFile = null;
-        if (StringUtil.isNotEmpty(reportDir) && (reportDir.startsWith("/") | reportDir.matches("(^[a-zA-Z][:][\\\\].+)"))) {
-            reportFile = new File(FilenameUtils.normalize(reportDir));
-        } else {
-            reportFile = new File(FilenameUtils.normalize(defaultRootDir
-                    + "/" + (reportDir == null ? "" : reportDir)));
-        }
-        try {
-            if (!reportFile.exists()) {
-                FileUtils.forceMkdir(reportFile);
-            }
-        } catch (FileNotFoundException fnfe) {
-            reportFile = new File(defaultRootDir, reportDir);
-        } catch (IOException e) {
-            reportFile = new File(defaultRootDir, reportDir);
-        } finally {
-            return reportFile;
-        }
-    }
-
-    public static void saveJunit(String report,
-                                 File junitDir,
-                                 String masterId,
-                                 BuildProgressLogger logger
-    ) {
-
-        File junitFile = new File(junitDir, masterId + ".xml");
-        try {
-            if (!junitFile.exists()) {
-                FileUtils.forceMkdir(junitFile.getParentFile());
-                junitFile.createNewFile();
-            }
-
-            BufferedWriter out = new BufferedWriter(new FileWriter(junitFile));
-            out.write(report);
-            out.close();
-            logger.message("Report was saved to " + junitFile.getAbsolutePath());
-        } catch (IOException e) {
-            logger.message("ERROR: Failed to save XML report to workspace " + e.getMessage());
-            logger.message("Unable to save XML report to workspace - check that test is finished on server or turn to support ");
-            logger.exception(e);
-        }
-    }
-
-
-    public static void sleep(int sleepPeriod, BuildProgressLogger logger) {
-        try {
-            Thread.currentThread().sleep(sleepPeriod);
-        } catch (InterruptedException e) {
-            logger.exception(e);
-            logger.warning("Test was interrupted during sleeping");
         }
     }
 }
