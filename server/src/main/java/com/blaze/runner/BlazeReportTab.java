7a6de1fd2027609b3fe06aa540b3dccfafb62fab
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-04-23 16:28:48 +0300

    add report tab logic

--- server/src/main/java/com/blaze/runner/BlazeReportTab.java
+++ server/src/main/java/com/blaze/runner/BlazeReportTab.java
@@ -2,18 +2,32 @@ package com.blaze.runner;
 
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
 import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.ViewLogTab;
+import jetbrains.buildServer.web.reportTabs.ReportTabUtil;
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Present BlazeMeter report tab in Build results
  */
 public class BlazeReportTab extends ViewLogTab {
+
+    private Logger logger = LoggerFactory.getLogger("com.blazemeter");
 
     /**
      * Creates and registers tab for Build Results pages
@@ -28,13 +42,47 @@ public class BlazeReportTab extends ViewLogTab {
 
     @Override
     protected void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request, @NotNull SBuild build) {
-        System.out.println();
-        model.put("url", "http://blazedemo.com");
+        BuildArtifact artifact = ReportTabUtil.getArtifact(build, Constants.RUNNER_DISPLAY_NAME + "/" + Constants.BZM_REPORTS_FILE);
+        if(artifact != null) {
+            try {
+                final Map<String, String> links = getReports(artifact.getInputStream());
+                if (!links.isEmpty()) {
+                    model.put("bzm.reports", links);
+                } else {
+                    model.put("bzm.msg", "There is no report for this build");
+                }
+
+            } catch (IOException e) {
+                logger.error("Failed to get the report: ", e);
+                model.put("bzm.msg", "Failed to get the report: " + e.getMessage());
+            }
+        } else {
+            logger.info("No BlazeMeter artifacts for this build");
+            model.put("bzm.msg", "No BlazeMeter artifacts for this build");
+        }
+    }
+
+    private Map<String, String> getReports(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final Map<String, String> links = new HashMap<>();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String key = line;
+            logger.debug("Get key: " + key);
+            if ((line = reader.readLine()) != null) {
+                logger.debug("Get address: " + line);
+                links.put(key, line);
+            } else {
+                logger.warn("Key " + key + " has not url");
+                links.put(key, "");
+            }
+        }
+        return links;
     }
 
     @Override
     protected boolean isAvailable(@NotNull HttpServletRequest request, @NotNull SBuild build) {
-        return true;
-//        return super.isAvailable(request, build);
+        return super.isAvailable(request, build) && ReportTabUtil.isAvailable(build, Constants.RUNNER_DISPLAY_NAME + "/" + Constants.BZM_REPORTS_FILE);
     }
 }
--- server/src/main/resources/buildServerResources/reportTab.jsp
+++ server/src/main/resources/buildServerResources/reportTab.jsp
@@ -1,20 +1,32 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<div class="successStatusBlock statusBlock" style="margin-top: 0.5em;">
-    <table class="statusTable">
-        <tbody>
-            <tr><td></td></tr>
-            <tr>
-                <td class="st">
-                    <span class="buildDataIcon">
-                        <span class="icon icon16 build-status-icon build-status-icon_successful"></span>
-                    </span>
-                    ${url}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<c:choose>
+    <c:when test="${bzm.msg != null}">
+        <div class="icon_before icon16 attentionComment" id="blazeWarningMessage">${bzm.msg}</div>
+    </c:when>
+    <c:otherwise>
+        <c:if test="${bzm.reports != null}">
+            <c:forEach items="${bzm.reports.keySet()}" var="key">
+                <div class="successStatusBlock statusBlock" style="margin-top: 0.5em;">
+                    <table class="statusTable">
+                        <tbody>
+                            <tr><td></td></tr>
+                            <tr>
+                                <td class="st">
+                                    <span class="buildDataIcon">
+                                        <span class="icon icon16 build-status-icon build-status-icon_successful"></span>
+                                    </span>
+                                    <a href="${bzm.reports.get(key)}">${key}</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </c:forEach>
+        </c:if>
+    </c:otherwise>
+</c:choose>
 
 
-<div class="icon_before icon16 attentionComment" id="blazeWarningMessage">${url}</div>
+
+
